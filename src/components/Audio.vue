4a0e4f2bf6e080a18d5d534d4ce87153affc699b
evvvritt <evv.info@gmail.com>
2016-08-22 20:23:01 +0200

    audio comp, label updating on hover

---
+++ src/components/Audio.vue
@@ -0,0 +1,20 @@
+<template lang="jade">
+  audio(
+        v-el:audio,
+        v-bind:src="audio",
+      )
+</template>
+
+<script>
+export default {
+  props: ['audio', 'play', 'autoplay', 'mute'],
+  watch: {
+    'play': function (play) {
+      play ? this.$els.audio.play() : this.$els.audio.pause()
+    },
+    'mute': function (mute) {
+      this.$els.audio.muted = mute
+    }
+  }
+}
+</script>
--- src/components/Player.vue
+++ src/components/Player.vue
@@ -3,15 +3,17 @@
   #player
     
     .controls
-      label {{controls.preview}}
-      button(v-on:click="audioPlay()") {{play ? 'pause' : 'play'}}
-      button(v-on:click="audioMute()") {{mute ? 'unmute' : 'mute'}}
-      button(v-on:click="matterjsClear()") clear
-      button(v-on:click="next()") next
+        label {{action}}
+        br
+        button(@click="pwrPlay", @mouseenter="btnEnter", @mouseleave="btnLeave") {{play ? 'pause' : 'play'}}
+        button(@click="audioMute", @mouseenter="btnEnter", @mouseleave="btnLeave") {{mute ? 'unmute' : 'mute'}}
+        button(@mouseenter="btnEnter", @mouseleave="btnLeave") clear
+        button(@mouseenter="btnEnter", @mouseleave="btnLeave") next
     
     audio(
-      v-el:audio,
-      v-bind:src="audio"
+      :audio="audio",
+      :play="play",
+      :mute="mute"
     )
     
     .text {{{text}}}
@@ -24,42 +26,42 @@
 </template>
 
 <script>
+import Audio from './Audio'
 export default {
+  components: {
+    Audio
+  },
   data () {
     return {
       play: false,
       mute: false,
-      controls: {
-        label: ''
-      },
+      action: '',
       scene: 'gravity',
       audio: '/static/demo/song1.mp3',
       text: 'pwrTalk is a consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     }
   },
   methods: {
-    audioPlay: function () {
-      if (!this.play) { // play
-        this.$els.audio.play()
-        this.play = true
-        // unmute
-        this.mute = false
-        this.$els.audio.muted = false
-      } else { // pause
-        this.$els.audio.pause()
-        this.play = false
-      }
+    pwrPlay: function (e) {
+      this.play = !this.play
+      this.mute = this.play ? false : this.mute
+      var vm = this
+      setTimeout(function () {
+        vm.action = e.target.innerHTML
+      }, 10)
     },
-    audioMute: function () {
+    audioMute: function (e) {
       this.mute = !this.mute
-      var audio = this.$els.audio
-      audio.muted = !audio.muted
+      var vm = this
+      setTimeout(function () {
+        vm.action = e.target.innerHTML
+      }, 10)
     },
-    matterjsClear: function () {
-      console.log('clean')
+    btnEnter: function (e) {
+      this.action = e.target.innerHTML
     },
-    next: function () {
-      console.log('next')
+    btnLeave: function () {
+      this.action = ''
     }
   }
 }
