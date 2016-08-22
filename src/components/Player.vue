6461a74346fc9df094d650078284e3eb29479d14
evvvritt <evv.info@gmail.com>
2016-08-22 20:44:21 +0200

    colors data, btns inline

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -5,10 +5,10 @@
     .controls
         label {{action}}
         br
-        button(@click="pwrPlay", @mouseenter="btnEnter", @mouseleave="btnLeave") {{play ? 'pause' : 'play'}}
-        button(@click="audioMute", @mouseenter="btnEnter", @mouseleave="btnLeave") {{mute ? 'unmute' : 'mute'}}
-        button(@mouseenter="btnEnter", @mouseleave="btnLeave") clear
-        button(@mouseenter="btnEnter", @mouseleave="btnLeave") next
+        button(@click="pwrPlay", @mouseenter="btnEnter", @mouseleave="btnLeave", v-bind:style="{ backgroundColor : colors.btns1 }") {{play ? 'pause' : 'play'}}
+        button(@click="audioMute", @mouseenter="btnEnter", @mouseleave="btnLeave", v-bind:style="{ backgroundColor : colors.btns2 }") {{mute ? 'unmute' : 'mute'}}
+        button(@mouseenter="btnEnter", @mouseleave="btnLeave", v-bind:style="{ backgroundColor : colors.btns1 }") clear
+        button(@mouseenter="btnEnter", @mouseleave="btnLeave", v-bind:style="{ backgroundColor : colors.btns2 }") next
     
     audio(
       :audio="audio",
@@ -38,7 +38,15 @@ export default {
       action: '',
       scene: 'gravity',
       audio: '/static/demo/song1.mp3',
-      text: 'pwrTalk is a consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      text: 'pwrTalk is a consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      colors: {
+        btns1: 'rgb(247, 221, 212)',
+        btns2: 'rgb(255,17,73)',
+        bg: 'rgb(252,67,111)',
+        gd1a: 'rgb(252,67,111)',
+        gd1b: 'rgb(113,187,161)',
+        gd1direction: 'top'
+      }
     }
   },
   methods: {
