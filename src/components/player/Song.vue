a32633a9934125612dd47877d41fcd7cf9d7b397
evvvritt <evv.info@gmail.com>
2016-10-09 17:06:39 +0200

    global style re-added, song.vue renamed from audio

---
+++ src/components/player/Song.vue
@@ -0,0 +1,17 @@
+<template lang="jade">
+  audio(ref="song",:src="song")
+</template>
+
+<script>
+export default {
+  props: ['song', 'play', 'autoplay', 'mute'],
+  watch: {
+    play: function (play) {
+      return play ? this.$els.song.play() : this.$els.song.pause()
+    },
+    mute: function (mute) {
+      this.$els.song.muted = mute
+    }
+  }
+}
+</script>
